9d5cb32c7fde68ee6d8c33e01321d360bc4dd896
Abir Ahmed <grabirahmed@gmail.com>
2026-04-08 23:35:58 +0600

    added btn code

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ function hideLoading() {
   loadingSpinner.classList.add("hidden");
 }
 
+
+// Category btn
+async function loadCategories() {
+  // async await
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/categories",
+  );
+  const data = await res.json();
+  //   console.log(data);
+  //   console.log(categoriesContainer);
+  data.categories.forEach((category) => {
+    // console.log(category);
+    const btn = document.createElement("button");
+    btn.className = "btn btn-outline w-full";
+    btn.textContent = category.category_name;
+    btn.onclick = () => selectCategory(category.id, btn);
+    categoriesContainer.appendChild(btn);
+  });
+}
+
